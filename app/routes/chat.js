eb72d5e70fda24efad911963091740a8bed99a12
Pedro Fernandes <pfernandes180103@gmail.com>
2024-03-24 18:48:40 -0300

    implementando rotas

---
+++ app/routes/chat.js
@@ -0,0 +1,8 @@
+module.exports = (application) => {
+    application.post('/chat', (req, res) => {
+        res.render('chat')
+    });
+    application.get('/chat', (req, res) => {
+        res.render('chat')
+    });
+}
---
+++ config/server.js
@@ -0,0 +1,37 @@
+/* importar o módulo do framework express */
+const express = require('express');
+
+/* importar o módulo do consign */
+const consign = require('consign');
+
+/* importar o módulo do body-parser */
+const bodyParser = require('body-parser');
+
+/* importar o módulo do express-validator */
+const expressValidator = require('express-validator');
+
+/* iniciar o objeto do express */
+const app = express();
+
+/* setar as variáveis 'view engine' e 'views' do express */
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+/* configurar o middleware express.static */
+app.use(express.static('./app/public'));
+
+/* configurar o middleware body-parser */
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*configurar o middleware express-validator */
+app.use(expressValidator());
+
+/* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
+/* exportar o objeto app */
+module.exports = app;
