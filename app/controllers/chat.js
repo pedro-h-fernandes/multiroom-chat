81e567710f63d2a2bd0498884fae13e2742cda4a
Pedro Fernandes <pfernandes180103@gmail.com>
2024-03-26 17:44:24 -0300

    recebendo e validando os dados do formulario

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,13 @@
 module.exports.iniciaChat = (application, req, res) => {
-    res.render('chat');
-}
+  const dadosForm = req.body;
+  req.assert("apelido", "Nome ou apelido é obrigatório").notEmpty();
+  req.assert("apelido", "Nome ou apelido deve conter entre 3 e 15 caracteres").len(3, 15);
+
+  const erros = req.validationErrors(); // Retorna um array de erros, caso haja
+
+  if (erros) {
+    res.render("index", { validacao: erros });
+    return;
+  }
+  res.render("chat");
+};
--- app/routes/chat.js
+++ app/routes/chat.js
@@ -3,6 +3,6 @@ module.exports = (application) => {
         application.app.controllers.chat.iniciaChat(application, req, res);
     });
     application.get('/chat', (req, res) => {
-        application.app.controllers.chat.iniciaChat(application, req, res);
+        application.app.controllers.chat.iniciaChat (application, req, res);
     });
 }
