93d79f1861ecdf7a7584ab367c80e14b76fe645e
Pedro Fernandes <pfernandes180103@gmail.com>
2024-03-26 22:41:23 -0300

    abrindo conexao via websocket

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ const server = app.listen(80, function(){
 })
 
 const io = require('socket.io')(server);
+
+//criar a conexao por websocket
+
+io.on('connection', function(socket){
+    console.log('Usuario conectou');
+
+    socket.on('disconnect', function(){
+        console.log('Usuario desconectou');
+    });
+
+    // socket.on('msgParaServidor', function(data){
+    //     socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+
+    //     socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+
+    //     if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+    //         socket.emit('participantesParaCliente', {apelido: data.apelido});
+    //         socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido});
+    //     }
+    // });
+});
