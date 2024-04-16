8becb2396ff901580d2474d15cf9e036020223cc
Pedro Fernandes <pfernandes180103@gmail.com>
2024-04-16 18:23:16 -0300

    atualizando a relação de participantes da conversa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const app = require('./config/server');
 
 /* parametrizar porta de escuta */
-const server = app.listen(3000, function(){
+const server = app.listen(3000, function () {
     console.log('Servidor online');
 })
 
@@ -12,21 +12,21 @@ app.set('io', io); // cria variavel global io pora ser acessada em qualquer luga
 
 //criar a conexao por websocket
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('Usuario conectou');
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('Usuario desconectou');
     });
 
-    socket.on('msgParaServidor', function(data){
-        socket.emit('msgParaCliente', {apelido: data.apelido, msg: data.msg});  //envia a msg pro cliente que enviou a msg
+    socket.on('msgParaServidor', function (data) {
+        socket.emit('msgParaCliente', { apelido: data.apelido, msg: data.msg });  //envia a msg pro cliente que enviou a msg
 
-        socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, msg: data.msg}); // envia para todos os usuarios do chat
+        socket.broadcast.emit('msgParaCliente', { apelido: data.apelido, msg: data.msg }); // envia para todos os usuarios do chat
 
-    //     if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
-    //         socket.emit('participantesParaCliente', {apelido: data.apelido});
-    //         socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido});
-    //     }
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit('participantesParaCliente', { apelido: data.apelido });
+            socket.broadcast.emit('participantesParaCliente', { apelido: data.apelido });
+        }
     });
 });
